0ee3f5febcba86c55283bde0325a5258e049ef09
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-05-04 14:56:12 +0200

    Das Inventar selber ist, wie besprochen, keine Klasse mehr.

--- src/Inventory.py
+++ src/Inventory.py
@@ -4,39 +4,39 @@
 
 _inventory = [] #Contains all items which are stored in fridge. Do NOT directly access _inventory!
 
-class Inventory:
-    def getInventory(self):
+def getInventory():
         return _inventory
 
-    def newItem(self, type, name, expiry, quantity, unit): #type can be "stackable" or "continous". unit can be "[]" or nothing if item is stackable. quantity must be 1 for stackable items.
-        if type == "stackable" or type == "continous":
-            if type == "stackable":
-                if quantity == 1 or quantity == "":
-                    list = [expiry]
-                    _inventory.append(StackableItem(name, list))
-                else:
-                    print("Error: You can ony add one stackable item at one time! quantity must be 1.")
-            if type == "continous":
+def newItem(type, name, expiry, quantity, unit): #type can be "stackable" or "continous". unit can be "[]" or nothing if item is stackable. quantity must be 1 for stackable items.
+    if type == "stackable" or type == "continous":
+        if type == "stackable":
+            if quantity == 1 or quantity == "":
                 list = [expiry]
-                _inventory.append(ContinuousItem(name, expiry, quantity, unit))
-        else:
-            print("Error: type {type} is unknown.")
+                _inventory.append(StackableItem(name, list))
+            else:
+                print("Error: You can ony add one stackable item at one time! quantity must be 1.")
+        if type == "continous":
+            list = [expiry]
+            _inventory.append(ContinuousItem(name, expiry, quantity, unit))
+    else:
+        print("Error: type {type} is unknown.")
 
-    def addItem(self, type, name, expiry, addend): #Increases quantity and adds properties to an existing type of item. addend is the added quantity for continous items or must be 1 for countable items. expiry can be nothing for continous items.
-        for i in range (len(_inventory) - 1):
-            if name == _inventory[i].name:
-                if type == "stackable" or type == "continous":
-                    if type == "stackable":
-                        if addend == 1 or addend == "":
-                            _inventory[i].add(expiry)
-                        else:
-                            print("Error: You can ony add one stackable item at one time! addend must be 1.")
-                    if type == "continous":
-                        _inventory[i].refill(addend)
-                else:
-                    print("Error: type {type} is unknown.")
+def addItem(type, name, expiry, addend): #Increases quantity and adds properties to an existing type of item. addend is the added quantity for continous items or must be 1 for countable items. expiry can be nothing for continous items.
+    for i in range (len(_inventory) - 1):
+        if name == _inventory[i].name:
+            if type == "stackable" or type == "continous":
+                if type == "stackable":
+                    if addend == 1 or addend == "":
+                        _inventory[i].add(expiry)
+                    else:
+                        print("Error: You can ony add one stackable item at one time! addend must be 1.")
+                if type == "continous":
+                    _inventory[i].refill(addend)
+            else:
+                print("Error: type {type} is unknown.")
 
 
+#classes for items
 #There will be 2 Types of items contained in inventory: StackableItem(s) and CountinousItem(s)
 
 class StackableItem:
