96ab14cda3b354d03e68d60ae2c2441c37f80820
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-05-04 11:53:13 +0200

    Main file created

--- src/iFridge.py
+++ src/iFridge.py
@@ -1,6 +1,5 @@
 #iFridge
 #API course
-#(c) Daniel Smigala, Yasmine Hildebrand, Constantin Denden, Julius Schieck, David Nagel
 
 #This is the main file of the iFridge project.
 #Please try to implement as much as possible as modules.
