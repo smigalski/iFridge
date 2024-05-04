b220c3f41f03a02240d8236eea520dbd22867be4
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-05-04 14:05:11 +0200

    Funktion addItem zu Inventory hinzugefügt. Fehler bei Aufruf von Listen krrigiert (Listenobjekt mit Index in eckigen Klammern aufrufen: [Index])

--- src/Inventory.py
+++ src/Inventory.py
@@ -5,21 +5,33 @@
 _inventory = [] #Contains all items which are stored in fridge. Do NOT directly access _inventory!
 
 class Inventory:
-    def getInventory:
+    def getInventory(self):
         return _inventory
 
-    def newItem(self, type, name, expiry,unit): #type can be "stackable" or "continous". unit can be "[]" or nothing if item is stackable
+    def newItem(self, type, name, expiry, quantity, unit): #type can be "stackable" or "continous". unit can be "[]" or nothing if item is stackable. quantity must be 1 for stackable items.
         if type == "stackable" or type == "continous":
             if type == "stackable":
                 list = [expiry]
                 _inventory.append(StackableItem(name, list))
             if type == "continous":
                 list = [expiry]
-                _inventory.append(ContinuousItem(name,quantity,unit))
+                _inventory.append(ContinuousItem(name, quantity, unit))
         else:
             print("Error: type {type} is unknown.")
 
-    def addItem(self, type, name, expiry): #Increases quantity and adds properties to an existing type of item. unit can be "[]" or nothing if item is stackable
+    def addItem(self, type, name, expiry, addend): #Increases quantity and adds properties to an existing type of item. addend is the added quantity for continous items or must be 1 for countable items. expiry can be nothing for continous items.
+        for i in range (len(_inventory) - 1):
+            if name == _inventory[i].name:
+                if type == "stackable" or type == "continous":
+                    if type == "stackable":
+                        if addend == 1 or addend == "":
+                            _inventory[i].add(expiry)
+                        else:
+                            print("Error: You can ony add one stackable item at one time! addend must be 1.")
+                    if type == "continous":
+                        _inventory[i].refill(addend)
+                else:
+                    print("Error: type {type} is unknown.")
 
 
 #There will be 2 Types of items contained in inventory: StackableItem(s) and CountinousItem(s)
@@ -40,7 +52,7 @@ class StackableItem:
 
     def remove(self, index):
         list = []
-        for i in range self.quantity:
+        for i in range (self.quantity - 1):
             if i != index:
                 list.append(self.expiries(i))
         self.expiries = list
