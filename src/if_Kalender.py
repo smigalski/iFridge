3ba7eba92cecc10d53aa24d35b4c20a74d0fe286
David Nagel <david.nagel@tu-braunschweig.de>
2024-05-06 22:54:51 +0200

    Hinzufügen des Unterprogramms if_Kalender.py zur Definition einer Klasse für Termine sowie einem Objekt für Kalender. Später: Hinzufügen einer Beschreibung eines Dateiformats für Kalender und  je einer Funktion zum einlesen und speichern dieser Dateien.

---
+++ src/if_Kalender.py
@@ -0,0 +1,25 @@
+#iFridge
+#API course summer 2024
+#(c) Daniel Smigala, Yasmine Hildebrand, Constantin Denden, Julius Schieck, David Nagel
+
+#Dieses Unterprogramm ermöglicht das Einlesen und Abspeichern von Kalenderdateien sowie das Hinzufügen von Terminen.
+
+#Die Terminklasse beinhaltet Datum (YYYY_MM_DD), Uhrzeit (HH_MM) und Namen des Termins.
+#Eine Kalenderobjekt beinhaltet den Namen des Users sowie eine Liste aus Terminen, Sortiert nach Datum und Uhrzeit.
+
+
+
+#Terminklasse erstellen
+class termin:
+    def __init__(self, datum, zeit, titel, event):
+        self.datum = datum                      #Datum eingeben als String in folgendem Format: YYYY_MM_DD
+        self.zeit = zeit                        #Zeit eingeben als String in folgendem Format als 24h Uhrzeit: HH_MM
+        self.titel = titel                      #Titel als String eingeben
+        self.event = event                      #Event als String eingeben
+
+class kalender:
+    def __init__(self, user, terminliste):
+        self.user = user
+        self.terminliste = [terminliste]
+    
+
