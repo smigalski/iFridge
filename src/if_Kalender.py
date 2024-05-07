87a6ffd8f805ec2158f095da0a8e3e4da6951e39
David Nagel <david.nagel@tu-braunschweig.de>
2024-05-07 16:07:13 +0200

    Einführende Kommentare in einem Kommentarblock zusammengefügt

--- src/if_Kalender.py
+++ src/if_Kalender.py
@@ -1,11 +1,13 @@
-#iFridge
-#API course summer 2024
-#(c) Daniel Smigala, Yasmine Hildebrand, Constantin Denden, Julius Schieck, David Nagel
+"""
+iFridge
+API course summer 2024
+(c) Daniel Smigala, Yasmine Hildebrand, Constantin Denden, Julius Schieck, David Nagel
 
-#Dieses Unterprogramm ermöglicht das Einlesen und Abspeichern von Kalenderdateien sowie das Hinzufügen von Terminen.
+Dieses Unterprogramm ermöglicht das Einlesen und Abspeichern von Kalenderdateien sowie das Hinzufügen von Terminen.
 
-#Die Terminklasse beinhaltet Datum (YYYY_MM_DD), Uhrzeit (HH_MM), Titel des Termins und eine Beschreibung.
-#Eine Kalenderobjekt beinhaltet den Namen des Users sowie eine Liste aus Terminen, Sortiert nach Datum und Uhrzeit.
+Die Terminklasse beinhaltet Datum (YYYY_MM_DD), Uhrzeit (HH_MM), Titel des Termins und eine Beschreibung.
+Eine Kalenderobjekt beinhaltet den Namen des Users sowie eine Liste aus Terminen, Sortiert nach Datum und Uhrzeit.
+"""
 
 #Importierte Module
 from operator import itemgetter
