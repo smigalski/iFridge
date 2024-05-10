22e54fd7cd6181c88e74ed5963331c0b07aa7650
Yasmine <Yasmine@DESKTOP-53V838D.local>
2024-05-10 12:12:57 +0200

    Löschen der Klasse Eiskaffee und der Importierten Module

--- src/Eiskaffee.py
+++ src/Eiskaffee.py
@@ -12,17 +12,9 @@ from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QFrame,
 #from pyqt5.qtgui import *
 #from pyqt5.uic import *
 import sys
-import Eiskaffee_front_page_2
+from Eiskaffee_front_page_2 import *
 
 # Dateipfad zum konvertieren QT zu PY: C:\Users\Yasmine\AppData\Local\Programs\Python\Python311\Scripts\pyuic5 -x *NameDate.ui* - o *NeuerNameDerDatei.py*
 
-class Eiskaffee:
-    def __init__(self, milch, kaffee, eiswuerfel):
-        self.milch = milch
-        self.kaffee = kaffee
-        self.eiswuerfel = eiswuerfel
-        self.eiskaffee = Eiskaffee(0, 0, 0)
 
-    def __str__(self):
-        return f"Eiskaffee: Milch={self.milch}ml, Kaffee={self.kaffee}ml, Eiswürfel={self.eiswuerfel} Stück"
 
