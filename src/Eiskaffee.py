bff4c7cde5933d434f8282ee3b942409f57b7721
Yasmine <Yasmine@DESKTOP-53V838D.local>
2024-05-10 10:37:44 +0200

    Neue Front-Page-2 erstellt

--- src/Eiskaffee.py
+++ src/Eiskaffee.py
@@ -14,7 +14,7 @@ from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QFrame,
 import sys
 import Eiskaffee_front_page
 
-# Dateipfad zum konvertieren QT zu PY: C:\Users\Yasmine\AppData\Local\Programs\Python\Python311\pyuic5 -x *NameDate.ui* - o *NeuerNameDerDatei.py*
+# Dateipfad zum konvertieren QT zu PY: C:\Users\Yasmine\AppData\Local\Programs\Python\Python311\Scripts\pyuic5 -x *NameDate.ui* - o *NeuerNameDerDatei.py*
 
 app = QApplication(sys.argv)
 w = loadUi("Eiskaffee_front_page.ui")         #Name der Frontpage zu w geändert
