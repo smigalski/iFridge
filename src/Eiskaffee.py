ecac7a76f01f1c05de553e6570c2a044d7822900
Yasmine <Yasmine@DESKTOP-53V838D.local>
2024-05-10 11:38:58 +0200

    Klasse Eiskaffee erstellt

--- src/Eiskaffee.py
+++ src/Eiskaffee.py
@@ -16,4 +16,13 @@ import Eiskaffee_front_page_2
 
 # Dateipfad zum konvertieren QT zu PY: C:\Users\Yasmine\AppData\Local\Programs\Python\Python311\Scripts\pyuic5 -x *NameDate.ui* - o *NeuerNameDerDatei.py*
 
+class Eiskaffee:
+    def __init__(self, milch, kaffee, eiswuerfel):
+        self.milch = milch
+        self.kaffee = kaffee
+        self.eiswuerfel = eiswuerfel
+        self.eiskaffee = Eiskaffee(0, 0, 0)
+
+    def __str__(self):
+        return f"Eiskaffee: Milch={self.milch}ml, Kaffee={self.kaffee}ml, Eiswürfel={self.eiswuerfel} Stück"
 
