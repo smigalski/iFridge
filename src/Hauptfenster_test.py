22726a6e19baf368e594c872e44d30cffd264c43
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-05-31 21:22:39 +0200

    Laden/öffnen aus der ui. Datei, anstatt das auf die Funktion def setupUI zuzugreifen, aber Befehl(Klicken auf pushButton und Öffnen des Nutzerfensters beibehalten). Funktioniert mit der Methode self.findChild

---
+++ src/Hauptfenster_test.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QPushButton
+
+from src.Nutzerfenster import Ui_Nutzerfenster
+
+
+class Ui_QMainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Ui_QMainWindow, self).__init__()
+        uic.loadUi('Hauptfenster.ui', self)
+
+        # Finde den Button aus der UI und verbinde ihn mit der Methode
+        self.button = self.findChild(QPushButton, 'pushButton')
+        self.button.clicked.connect(self.openNutzerfenster)
+
+
+
+    def openNutzerfenster(self):
+        self.window = QtWidgets.QWidget()
+        self.ui = Ui_Nutzerfenster()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui_QMainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ src/test.py
@@ -0,0 +1,47 @@
+import sys
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QColor
+
+
+
+class RGBSlider(QtWidgets.QWidget):
+    def __init__(self):
+        super(RGBSlider, self).__init__()
+        uic.loadUi('RGB_Einstellungen.ui', self)
+
+        # Verbinde die Slider mit der updateColor Methode
+        self.Slider_rot1.valueChanged.connect(self.updateColor)
+        self.Slider_gruen1.valueChanged.connect(self.updateColor)
+        self.Slider_blau1.valueChanged.connect(self.updateColor)
+
+        self.Slider_rot2.valueChanged.connect(self.updateColor)
+        self.Slider_gruen2.valueChanged.connect(self.updateColor)
+        self.Slider_blau2.valueChanged.connect(self.updateColor)
+
+        # Initiale Farbanzeige
+        self.updateColor()
+
+    def updateColor(self):
+        red1 = self.Slider_rot1.value()
+        green1 = self.Slider_gruen1.value()
+        blue1 = self.Slider_blau1.value()
+
+        red2 = self.Slider_rot2.value()
+        green2 = self.Slider_gruen2.value()
+        blue2 = self.Slider_blau2.value()
+
+        color1 = QColor(red1, green1, blue1)
+        colorString1 = color1.name()
+
+        color2 = QColor(red2, green2, blue2)
+        colorString2 = color2.name()
+
+        self.labelColor1.setStyleSheet(f"background-color: {colorString1};  color: white;  padding: 10px;  border-radius: 15px;")
+        self.labelColor2.setStyleSheet(f"background-color: {colorString2};  color: white;  padding: 10px;  border-radius: 15px;")
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = RGBSlider()
+    window.show()
+    sys.exit(app.exec_())
