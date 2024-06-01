00197d2150d6893983dccbdaa5cd400b0d92fc8e
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-06-01 13:21:58 +0200

    Neue Methode, um ui. Dateien zu öffnen, implementiert. Fenster (Hauptfenster --> Nutzerfenster --> Desktopfenster --> Allgemeine Einstellungen --> RGB Einstellungen) verbunden per QPushButton. Im Desktopfenster.py Platz und Hinweis gelassen für das Einfügen der restlichen Methoden.

--- src/Allgemeine_Einstellungen.py
+++ src/Allgemeine_Einstellungen.py
@@ -1,77 +1,23 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QPushButton
 
+from src.RGB_Einstellungen import RGBSlider
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(800, 800)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../img/icon_Einstellungen.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        Form.setWindowIcon(icon)
-        self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(0, 0, 801, 811))
-        self.label.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("../img/iFridge_klein.png"))
-        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
-        self.label.setObjectName("label")
-        self.horizontalScrollBar = QtWidgets.QScrollBar(Form)
-        self.horizontalScrollBar.setGeometry(QtCore.QRect(80, 110, 191, 631))
-        self.horizontalScrollBar.setOrientation(QtCore.Qt.Vertical)
-        self.horizontalScrollBar.setObjectName("horizontalScrollBar")
-        self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(50, 70, 261, 20))
-        self.label_2.setStyleSheet("font: 12pt \"Terminal\";\n"
-"")
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setGeometry(QtCore.QRect(10, 760, 221, 31))
-        self.label_3.setStyleSheet("font: 75 12pt \"Terminal\";")
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(Form)
-        self.label_4.setGeometry(QtCore.QRect(440, 220, 201, 20))
-        self.label_4.setStyleSheet("font: 12pt \"Terminal\";")
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
-        self.dial = QtWidgets.QDial(Form)
-        self.dial.setGeometry(QtCore.QRect(350, 250, 371, 361))
-        self.dial.setMaximum(10)
-        self.dial.setObjectName("dial")
-        self.label_5 = QtWidgets.QLabel(Form)
-        self.label_5.setGeometry(QtCore.QRect(350, 620, 221, 31))
-        self.label_5.setStyleSheet("font: 75 12pt \"Terminal\";")
-        self.label_5.setObjectName("label_5")
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(610, 30, 151, 81))
-        self.pushButton.setStyleSheet("font: 75 9pt \"Terminal\";\n"
-"background-color: rgb(255, 255, 255);\n"
-"\n"
-"")
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("../img/icon_RGB_Einstellungen.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon1)
-        self.pushButton.setIconSize(QtCore.QSize(48, 48))
-        self.pushButton.setObjectName("pushButton")
+class Ui_AllgemeineEinstellungen(QtWidgets.QWidget):
+    def __init__(self):
+        super(Ui_AllgemeineEinstellungen, self).__init__()
+        uic.loadUi('Allgemeine_Einstellungen.ui', self)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.button = self.findChild(QPushButton, 'pushButton')
+        self.button.clicked.connect(self.openRGB_Einstellungen)
 
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Allgmeine Einstellungen"))
-        self.label_2.setText(_translate("Form", "Temperatur in Celsius"))
-        self.label_3.setText(_translate("Form", "Eingestellt:"))
-        self.label_4.setText(_translate("Form", "Helligkeit"))
-        self.label_5.setText(_translate("Form", "Eingestellt:"))
-        self.pushButton.setText(_translate("Form", "RGB Einstellungen"))
-
+    def openRGB_Einstellungen(self):
+        self.window = RGBSlider()
+        self.window.show()
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
+    window = Ui_AllgemeineEinstellungen()
+    window.show()
     sys.exit(app.exec_())
--- src/Hauptfenster.py
+++ src/Hauptfenster.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QPushButton
 
 from src.Nutzerfenster import Ui_Nutzerfenster
 
@@ -9,73 +10,17 @@ class Ui_QMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui_QMainWindow, self).__init__()
         uic.loadUi('Hauptfenster.ui', self)
-        self.setupUi()
 
-    def setupUi(self):
-        self.setObjectName("QMainWindow")
-        self.resize(800, 800)
-        self.centralwidget = QtWidgets.QWidget(self)
-        self.centralwidget.setObjectName("centralwidget")
-        # noinspection PyArgumentList
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget, clicked = lambda: self.openNutzerfenster())
-        self.pushButton.setGeometry(QtCore.QRect(340, 60, 121, 61))
-        self.pushButton.setStyleSheet("font: 700 12pt \"Terminal\";\n"
-"background-color: rgb(255, 255, 255);")
-        self.pushButton.setDefault(False)
-        self.pushButton.setObjectName("pushButton")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(220, 80, 131, 31))
-        self.label_3.setStyleSheet("font: 700 9pt \"Terminal\";")
-        self.label_3.setObjectName("label_3")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(0, 40, 801, 801))
-        self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("../img/Kühlschrank_Bild.jpg.png"))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(0, 0, 791, 801))
-        self.label_2.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.label_2.setObjectName("label_2")
-        self.label_2.raise_()
-        self.label.raise_()
-        self.pushButton.raise_()
-        self.label_3.raise_()
-        self.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(self)
-        self.statusbar.setObjectName("statusbar")
-        self.setStatusBar(self.statusbar)
-
-        self.retranslateUi(self)
-        QtCore.QMetaObject.connectSlotsByName(self)
-
+        # Finde den Button aus der UI und verbinde ihn mit der Methode
+        self.button = self.findChild(QPushButton, 'pushButton')
+        self.button.clicked.connect(self.openNutzerfenster)
 
     def openNutzerfenster(self):
-        self.window = QtWidgets.QWidget()
-        self.ui = Ui_Nutzerfenster()
-        self.ui.setupUi(self.window)
+        self.window = Ui_Nutzerfenster()
         self.window.show()
-
-
-
-
-    def retranslateUi(self, QMainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        QMainWindow.setWindowTitle(_translate("QMainWindow", "iFridge"))
-        self.pushButton.setText(_translate("QMainWindow", "iFridge"))
-        self.label_3.setText(_translate("QMainWindow", "click here ------> "))
-        self.label_2.setText(_translate("QMainWindow", "TextLabel"))
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = Ui_QMainWindow()
     window.show()
     sys.exit(app.exec_())
-
-#if __name__ == "__main__":
-    #app = QtWidgets.QApplication(sys.argv)
-    #QMainWindow = QtWidgets.QMainWindow()
-    #ui = Ui_QMainWindow()
-    #ui.setupUi(QMainWindow)
-    #QMainWindow.show()
-    #sys.exit(app.exec_())
