b5e8818dabe65ec8c365177f4f33044bd7e239ea
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-06-18 19:42:21 +0200

    Laden des Inventars bei Programmstart in iFridge.py implementiert. Vorbereitung für Laden der Benutzer bei Programmstart in iFridge.py

--- src/iFridge.py
+++ src/iFridge.py
@@ -6,6 +6,43 @@
 #Please try to implement as much as possible as modules.
 #This file should only contain the superior procedures.
 
+
+#Modules:
+
 import Inventory
+
+
+#Config:
+
+inventorypath = "Inventory"
+userspath = "Users"
+
+
+
+#Procedures:
+
+try:
+    Inventory.ImportInventory(inventorypath)
+    print("Inventory loaded from " + inventorypath + " ." )
+except:
+    print("Error: An Error occoured while loading inventory from " + inventorypath + " .")
+
+
+
+
+#Hauptmenü öffnen
+
+
+
+
+#Inventory.ExportInventory(inventorypath)
+
+
+
+
+
+
+
+
 
 
