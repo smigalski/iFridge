6982d138307b67314bb7b9145007a6289ef3b0f2
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-06-18 19:45:51 +0200

    Fehlerbehandlung bei Inventar Laden ausführlicher

--- src/iFridge.py
+++ src/iFridge.py
@@ -25,7 +25,7 @@ try:
     Inventory.ImportInventory(inventorypath)
     print("Inventory loaded from " + inventorypath + " ." )
 except:
-    print("Error: An Error occoured while loading inventory from " + inventorypath + " . Ignore this error if this is the first run on this device or after an reset to factory state.")
+    print("Error: An Error occoured while loading inventory from >" + inventorypath + "<. Ignore this error if this is the first run on this device or after an reset to factory state.")
 
 
 
