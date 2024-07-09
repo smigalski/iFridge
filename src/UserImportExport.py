f2357f92ffc0fe6f3068edbfebdbfef426c13aac
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-07-09 15:26:58 +0200

    Funktion ExportUsers() fertig

--- src/UserImportExport.py
+++ src/UserImportExport.py
@@ -2,6 +2,25 @@
 #API course
 #(c) Daniel Smigala, Yasmine Hildebrand, Constantin Denden, Julius Schieck, David Nagel
 
+import ui_Usermanagement
+
+UserManagement = ui_Usermanagement.UserManagementInstanz
+
+
+def UsersToString():
+    usersstring = []
+    users = UserManagement.get_all_users()
+    index = 0
+    while (index < len(users)):
+        usersstring.append(str(users[index]["name"]) + ";" + str(users[index]["balance"]) + ";" + "/n")
+        index += 1
+
+def ExportUsers(path = "Users.txt"):
+    file = open(path, "w")
+    file.write(UsersToString(), "w")
+    file.close()
+
+
 
 
 
