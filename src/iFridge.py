2e0f65e09d46a0606886764b3f4e70aaf0b45192
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-07-09 14:00:57 +0200

    Methode, um aus iFridge.py Hauptfenster.py zu öffnen, Eiskaffee-Button erstellt.

--- src/iFridge.py
+++ src/iFridge.py
@@ -1,15 +1,20 @@
 #iFridge
 #API course
 #(c) Daniel Smigala, Yasmine Hildebrand, Constantin Denden, Julius Schieck, David Nagel
-
 #This is the main file of the iFridge project.
 #Please try to implement as much as possible as modules.
 #This file should only contain the superior procedures.
 
 
 #Modules   -------------------------------------
+import sys
+from PyQt5 import QtWidgets
 
+
+#Import Funktionen, Klassen von .py Dateien
 import Inventory
+from Hauptfenster import Ui_QMainWindow
+
 
 
 #Config   -------------------------------------
@@ -17,11 +22,10 @@ import Inventory
 inventorypath = "Storage/Inventory"
 userspath = "Storage/Users"
 
-
-
 #Procedures   -------------------------------------
 
-#Load inventory and users:
+
+#Import des Inventory:
 try:
     Inventory.ImportInventory(inventorypath)
     print("Inventory loaded from >" + inventorypath + "<." )
@@ -35,11 +39,12 @@ except:
 #   print("Error: An Error occoured while loading users from >" + userspath + "<. Ignore this error if this is the first run on this device or after an reset to factory state.")
 
 
+# Start des Hauptfenster.py
+app = QtWidgets.QApplication(sys.argv)
 
-
-#show main menu:
-
-
+Hauptfenster = Ui_QMainWindow()
+Hauptfenster.show()
+Hauptfenster.openMainWindow()
 
 
 
@@ -48,3 +53,7 @@ Inventory.ExportInventory(inventorypath)
 print("Inventory successfully exported to >" + inventorypath + "<.")
 #Inventory.ExportUsers(userspath)
 #print("Users successfully exported to >" + userspath + "<.")
+
+
+#Die Ui schließen
+sys.exit(app.exec_())
