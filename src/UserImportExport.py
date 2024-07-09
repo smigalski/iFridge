6e2f22a84147c7f332ddcb5f2a9f0e2286d52eef
Julius Schieck <j.schieck@tu-braunschweig.de>
2024-07-09 15:29:14 +0200

    Fehler in ExportUsers(path) und UsersToString() korrigiert.

--- src/UserImportExport.py
+++ src/UserImportExport.py
@@ -8,16 +8,17 @@ UserManagement = ui_Usermanagement.UserManagementInstanz
 
 
 def UsersToString():
-    usersstring = []
+    usersstring = ""
     users = UserManagement.get_all_users()
     index = 0
     while (index < len(users)):
         usersstring.append(str(users[index]["name"]) + ";" + str(users[index]["balance"]) + ";" + "/n")
         index += 1
+    return usersstring
 
 def ExportUsers(path = "Users.txt"):
     file = open(path, "w")
-    file.write(UsersToString(), "w")
+    file.write(UsersToString())
     file.close()
 
 
