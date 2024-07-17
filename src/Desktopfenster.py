474a73b653aaf76e44be0a8b75a36a7d99a7d03f
Daniel Smigala <d.smigala@tu-braunschweig.de>
2024-07-17 17:15:13 +0200

    Restliche Buttons des Desktopfenster mit den entsprechenden UI verbunden.

--- src/Desktopfenster.py
+++ src/Desktopfenster.py
@@ -20,6 +20,15 @@ class Ui_Desktopfenster(QtWidgets.QWidget):
         self.buttonEiskaffee = self.findChild(QPushButton, "pushButtonEiskaffee")
         self.buttonEiskaffee.clicked.connect(self.btnEiskaffee)
 
+        self.buttonInventory = self.findChild(QPushButton, "pushButtonInventory")
+        self.buttonInventory.clicked.connect(self.btnInventory)
+
+        self.buttonKalender = self.findChild(QPushButton, "pushButtonKalender")
+        self.buttonKalender.clicked.connect(self.btnKalender)
+
+        self.buttonNutzer = self.findChild(QPushButton, "pushButtonNutzerverw")
+        self.buttonNutzer.clicked.connect(self.btnNutzerverwaltung)
+
         self.backbutton = self.findChild(QPushButton, 'backButton')
         self.backbutton.clicked.connect(self.back_button_clicked)
 
@@ -29,6 +38,15 @@ class Ui_Desktopfenster(QtWidgets.QWidget):
 
     def btnEiskaffee(self):
         subprocess.run(['python', 'Eiskaffee_front_page_2.py'])
+
+    def btnInventory(self):
+        subprocess.run(['python', 'Einkaufsmanagement.py'])
+
+    def btnKalender(self):
+        subprocess.run(['python', 'if_Kalender.py'])
+
+    def btnNutzerverwaltung(self):
+        subprocess.run(['python', 'ui_Usermanagement.py'])
 
     def back_button_clicked(self):
         self.main_window.openNutzerfenster()
